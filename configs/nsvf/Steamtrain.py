3cabc3992157e0fd800a78f80aa297af78be53dd
hbell99 <denghuayu86@163.com>
2022-07-12 11:00:37 +0800

    nsvf configs

--- configs/nsvf/Steamtrain.py
+++ configs/nsvf/Steamtrain.py
@@ -4,9 +4,27 @@ expname = 'dvgo_Steamtrain'
 basedir = './logs/nsvf_synthetic'
 
 data = dict(
+    down=4,
     datadir='./data/Synthetic_NSVF/Steamtrain',
     dataset_type='nsvf',
     inverse_y=True,
     white_bkgd=True,
 )
+
+
+coarse_train = dict(
+    N_iters=0,
+)
+
+fine_model_and_render = dict(
+    # posbase_pe=10,
+    use_coarse_geo=False,
+)
+
+fine_train = dict(
+    N_iters=10000,
+    # lrate_k0=0, 
+    # ray_sampler='random',
+)
+
 
--- configs/nsvf/Toad.py
+++ configs/nsvf/Toad.py
@@ -4,9 +4,26 @@ expname = 'dvgo_Toad'
 basedir = './logs/nsvf_synthetic'
 
 data = dict(
+    down=1,
     datadir='./data/Synthetic_NSVF/Toad',
     dataset_type='nsvf',
     inverse_y=True,
     white_bkgd=True,
+)
+
+
+coarse_train = dict(
+    N_iters=0,
+)
+
+fine_model_and_render = dict(
+    # posbase_pe=10,
+    use_coarse_geo=False,
+)
+
+fine_train = dict(
+    N_iters=10000,
+    # lrate_k0=0, 
+    # ray_sampler='random',
 )
 
